a7c8e310d0c7cca280a662fa0d53fe3258a8cebe
juansmacbook <jc.lopez@uniandes.edu.co>
2022-09-09 19:57:55 +0200

    Push

--- ejercicio28.cpp
+++ ejercicio28.cpp
@@ -9,10 +9,10 @@ double fx(double velocidadx, double velocidady);
 double fy(double velocidadx, double velocidady);
 
 int main(){
-    
-    int n=500; /*Numero de iteracioens para el rk4*/
+
+    int n=500; /*Num of iterations for the rk4*/
+    double m=1; /*Mass in kilograms*/
     rk4(n);
-    
     return 0;
 }
 
@@ -36,40 +36,40 @@ void rk4(int n){
     for(int i=0;i<n;i++){
         
     /*Actualizamos los tamaños de los pasos para X y para Y*/
-    double k1x=h*fx(VX[i],VY[i]); 
-    double k1y=h*fy(VX[i],VY[i]);
+    double k1x=fx(VX[i],VY[i]);
+    double k1y=fy(VX[i],VY[i]);
         
-    double k2x=h*fx(VX[i]+0.5*k1x,VY[i]+0.5*k1y);
-    double k2y=h*fy(VX[i]+0.5*k1x,VY[i]+0.5*k1y);   
+    double k2x=fx(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y);
+    double k2y=fy(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y);
         
-    double k3x=h*fx(VX[i]+0.5*k2x,VY[i]+0.5*k2y);
-    double k3y=h*fy(VX[i]+0.5*k2x,VY[i]+0.5*k2y);   
+    double k3x=fx(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y);
+    double k3y=fy(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y);
         
-    double k4x=h*fx(VX[i]+k3x,VY[i]+k3y);
-    double k4y=h*fy(VX[i]+k3x,VY[i]+k3y);
+    double k4x=fx(VX[i]+h*k3x,VY[i]+h*k3y);
+    double k4y=fy(VX[i]+h*k3x,VY[i]+h*k3y);
         
-    double k5x=h*VX[i];
-    double k6x=h*(VX[i]+0.5*k1x);
-    double k7x=h*(VX[i]+0.5*k2x);
-    double k8x=h*(VX[i]+k3x);
+    double k5x=VX[i];
+    double k6x=(VX[i]+0.5*h*k1x);
+    double k7x=(VX[i]+0.5*h*k2x);
+    double k8x=(VX[i]+h*k3x);
 
-    double k5y=h*VY[i];
-    double k6y=h*(VY[i]+0.5*k1y);
-    double k7y=h*(VY[i]+0.5*k2y);
-    double k8y=h*(VY[i]+k3y);
-    /*Añadimos los pasos siguientes*/
-    X[i+1]=X[i]+(k5x+2*k6x+2*k7x+k8x)/6;
-    VX[i+1]=VX[i]+(k1x+2*k2x+2*k3x+k4x)/6;    
-    Y[i+1]=Y[i]+(k5y+2*k6y+2*k7y+k8y)/6;
-    VY[i+1]=Y[i]+(k1y+2*k2y+2*k3y+k4y)/6;
+    double k5y=VY[i];
+    double k6y=(VY[i]+0.5*h*k1y);
+    double k7y=(VY[i]+0.5*h*k2y);
+    double k8y=(VY[i]+h*k3y);
+    /*We calculate the next steps*/
+    X[i+1]=X[i]+h*(k5x+2*k6x+2*k7x+k8x)/6;
+    VX[i+1]=VX[i]+h*(k1x+2*k2x+2*k3x+k4x)/6;
+    Y[i+1]=Y[i]+h*(k5y+2*k6y+2*k7y+k8y)/6;
+    VY[i+1]=Y[i]+h*(k1y+2*k2y+2*k3y+k4y)/6;
     
     std::cout<<X[i]<<" "<<Y[i]<<std::endl;
     }
 }
 
 double fx(double velocidadx, double velocidady){
-    return -0.7*(velocidadx*velocidadx)/sqrt((velocidadx*velocidadx)+(velocidady*velocidady));
+    return -0.7*(1/m)*(velocidadx*velocidadx)/sqrt((velocidadx*velocidadx)+(velocidady*velocidady));
 }
 double fy(double velocidadx, double velocidady){
-    return -10-0.7*(velocidady*velocidady)/sqrt((velocidadx*velocidadx)+(velocidady*velocidady));
+    return -10-0.7*(1/m)*(velocidady*velocidady)/sqrt((velocidadx*velocidadx)+(velocidady*velocidady));
 }
