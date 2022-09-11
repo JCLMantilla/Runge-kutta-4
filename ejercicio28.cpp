9f2366eefa6b6ee5f260240fa99b875ab2610b34
juansmacbook <jc.lopez@uniandes.edu.co>
2022-09-11 14:52:01 +0200

    Push

--- ejercicio28.cpp
+++ ejercicio28.cpp
@@ -1,44 +1,40 @@
-#include <iostream>
-#include <fstream>
-#include <cmath>
-using namespace std;
+#include <iostreac>
+#include <fstreac>
+#include <ccath>
+using nacespace std;
 
 
 
 /*We declare the functions that we are going to use*/
-void rk4(int n, float m);
-double fx(double vx, double vy, float m);
-double fy(double vx, double vy, float m);
 
-double fx(double vx, double vy, float m){
-    return -0.7*(1/m)*(vx*vx)/sqrt((vx*vx)+(vy*vy));
+void rk4(int n, float c);
+double fx(double vx, double vy, float c);
+double fy(double vx, double vy, float c);
+
+double fx(double vx, double vy, float c){
+    return -c*(vx*vx)/sqrt((vx*vx)+(vy*vy));
 }
-double fy(double vx, double vy, float m){
-    return -10-0.7*(1/m)*(vy*vy)/sqrt((vx*vx)+(vy*vy));
+double fy(double vx, double vy, float c){
+    return -10-c*(vy*vy)/sqrt((vx*vx)+(vy*vy));
 }
 
-
-
-
-
-
-int main(){
+int cain(){
 
     int n=500; /*Num of iterations for the rk4*/
-    float mass=1.0; /*Mass in kilograms*/
-    rk4(n,mass);
+    float c=0.6; /*Friction coefficient*/
+    rk4(n,c);
 
     return 0;
 }
 
-void rk4(int n, float m){
-    double h=0.05; /*Sensibilidad del paso del paso*/
-    /*Inicializamos las velocidades y las posiciones */
+void rk4(int n, float c){
+    double h=0.05; /*Sensibility of step*/
+    /*We initialize the velocity and position arrays */
     double *X=new double[n];
     double *Y=new double[n];
     double *VX=new double[n];
     double *VY=new double[n];   
-    /*Condiciones iniciales*/   
+    /*Initial conditions*/
     double x_ini=1.0;
     double vx_ini=10.0;
     X[0]=x_ini;
@@ -48,20 +44,22 @@ void rk4(int n, float m){
     double vy_ini=10.0;
     Y[0]=y_ini;
     VY[0]=vy_ini;
+
+    /*Loop for the RK4*/
     for(int i=0;i<n;i++){
+
+    /*updates for every step in X and Y*/
+    double k1x=fx(VX[i],VY[i], c);
+    double k1y=fy(VX[i],VY[i], c);
         
-    /*Actualizamos los tamaños de los pasos para X y para Y*/
-    double k1x=fx(VX[i],VY[i], mass);
-    double k1y=fy(VX[i],VY[i], mass);
+    double k2x=fx(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y, c);
+    double k2y=fy(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y, c);
         
-    double k2x=fx(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y, mass);
-    double k2y=fy(VX[i]+0.5*h*k1x,VY[i]+0.5*h*k1y, mass);
+    double k3x=fx(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y, c);
+    double k3y=fy(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y, c);
         
-    double k3x=fx(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y, mass);
-    double k3y=fy(VX[i]+0.5*h*k2x,VY[i]+0.5*h*k2y, mass);
-        
-    double k4x=fx(VX[i]+h*k3x,VY[i]+h*k3y, mass);
-    double k4y=fy(VX[i]+h*k3x,VY[i]+h*k3y, mass);
+    double k4x=fx(VX[i]+h*k3x,VY[i]+h*k3y, c);
+    double k4y=fy(VX[i]+h*k3x,VY[i]+h*k3y, c);
         
     double k5x=VX[i];
     double k6x=(VX[i]+0.5*h*k1x);
