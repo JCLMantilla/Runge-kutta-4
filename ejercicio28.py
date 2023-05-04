fe6084b920f938cfefd7bc26bed2b9286ad1d995
juansmacbook <jc.lopez@uniandes.edu.co>
2023-05-04 09:21:06 +0200

    Added venv

--- ejercicio28.py
+++ ejercicio28.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 data1=np.loadtxt("ejercicio28.dat", encoding='UTF-8')
 data2=np.loadtxt("ejercicio28_sin_friccion.dat", encoding='UTF-8')
 
-
-
 plt.figure()
 plt.plot(data1[:,0], data1[:,1],label="friction")
 plt.plot(data2[:,0], data2[:,1],label="fricionless")
